ca0eca25ef35ac8efb69efe202ed118dc0a5fcbd
xbohov01 <32133115+xbohov01@users.noreply.github.com>
2017-09-25 19:22:15 +0200

    Add files via upload
    first draft for scanner header file, will be updated

---
+++ scanner.h
@@ -0,0 +1,24 @@
+/****** scanner.h ******/
+
+
+
+#define N_KEYWORDS 35
+#define N_OPERATOR_CHARS 8
+#define N_OPERATORS 12
+#define N_DELIMITERS 7
+
+typedef enum{
+
+} tokenType;
+
+struct{
+
+} token;
+
+//buffer for identifiers
+string *buffer;
+
+//function declarations
+int start_scanner(char *filename);
+int stop_scanner();
+int get_token();
