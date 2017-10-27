7acf85082a959b459462099a6028497cb7e70b83
Samo <xbohov01@stud.fit.vutbr.cz>
2017-10-27 12:43:56 +0200

    Initial commit

--- parser.c
+++ parser.c
@@ -1,1 +1,25 @@
 /****parser.c****/
+
+#include "parser.h"
+#include "ifj2017lex.h"
+
+int functions(){
+  
+}
+
+
+//<s> -> <funkcie> <scope>
+int start(){
+  switch (currentToken) {
+    case :
+  }
+}
+
+
+int start_parsing(){
+
+  int result;
+
+  result = start();
+
+}
--- parser.h
+++ parser.h
@@ -1,1 +1,10 @@
-/****parser.h****/
+/****** parser.h ******
+*
+* FIT VUT IFJ 2017/18
+* By Samuel Bohovic, xbohov01
+*
+*/
+
+tToken currentToken;
+
+int start_parsing();
