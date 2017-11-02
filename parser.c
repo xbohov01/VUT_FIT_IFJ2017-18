37d25024816e9694513315299f9604bf6003dffc
Samo <xbohov01@stud.fit.vutbr.cz>
2017-11-02 17:54:44 +0100

    added function argument rules

--- parser.c
+++ parser.c
@@ -21,6 +21,10 @@
 }
 
 int end_of_lines(){
+  while (currentToken.type == EOL){
+    NEXTT();
+  }
+  return SUCCESS;
 
 }
 
@@ -29,8 +33,42 @@ int var_declr(){
 
 }
 
-//<paramlist> ->	<deklaracia>	,	<paramlist>
+//<fncarg> -> <id>	as 	<type>
+//TODO add to symtable
+int fnc_arg(){
+  //already got identifier
+  NEXTT()
+
+  //expecting as keyword
+  CHECKT(AS_KEY);
+  NEXTT();
+
+  //expecting type
+  if (currentToken.type != (STRING_KEY || INTEGER_KEY || DOUBLE_KEY)){
+    return SYNT_ERR;
+  } else {
+    //gets next for condition in fnc_arglist
+    NEXTT();
+    return SUCCESS;
+  }
+
+}
+
+//<paramlist> ->	<fncarg>	,	<paramlist>
 int fnc_arglist(){
+
+  while (currentToken.type != TT_RIGHTPAR){
+    //has identifier or comma
+    if (currentToken.type == IDENTIFICATOR){
+      if (fnc_arg() != SUCCESS){
+        return SYNT_ERR;
+      }
+    } else if (currentToken.type == TT_COMMA){
+      NEXTT();
+    } else {
+      return SYNT_ERR;
+    }
+  }
 
 }
 
@@ -60,7 +98,8 @@ int functions(){
       return SYNT_ERR;
     } else {
       CHECKT(TT_RIGHTPAR);
-      return SUCCESS;
+      //expecting EOL(s) before next statement
+      return end_of_lines();
     }
   } else {
     return SYNT_ERR;
