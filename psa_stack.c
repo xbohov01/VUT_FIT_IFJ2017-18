460b7810d7a05e47de0ab1cf93df8a9efde0795c
Torchlight-stud <32226600+Torchlight-stud@users.noreply.github.com>
2017-11-20 22:00:29 +0100

    Partitially working expression parser, segmentation fault happens due errors in old scanner.c
    To try this, ask owner for Makefile of Gods.

--- psa_stack.c
+++ psa_stack.c
@@ -4,7 +4,7 @@
 **          Samuel Bohovic, xbohov01
 */
 
-#include "ifj2017.h"
+#include "psa_stack.h"
 
 // Additional options
 // =======================
@@ -213,16 +213,18 @@ T_NT_item* insert_after_T_NT(T_NT_stack *s, Data_Term *in_term, Data_NTerm *in_n
     return item_after;
 }
 
-// TODO: extend this function
+// TODO: extend this function 
 void error_exit(int code) {
     extern T_NT_stack *processing_stack;
     extern T_NT_stack *evaluation_stack;
     if (code == INTERNAL_ERR) {
         printf("CHYBA PAMETI\n");
     }
+    printf("\n");
     free_sources();
     destroy_T_NT_stack(processing_stack);
     destroy_T_NT_stack(evaluation_stack);
+    printf("Result: %d\n", code);
     exit(code);
 }
 
