f4192da044ea65edc95060c7611e387ab6c378eb
Danil <xgrigo02@stud.fit.vutbr.cz>
2017-11-24 19:02:13 +0100

    Zero char end added to func params.

--- parser.c
+++ parser.c
@@ -693,8 +693,8 @@ int functions(){
       strcpy(tmp_func_item->param_types, params.content);
       addchar('\0', tmp_func_item->param_types);
     } else {
-      tmp_func_item->param_types = malloc(sizeof(char));
-      strcpy(tmp_func_item->param_types, '\0');
+      tmp_func_item->param_types = malloc(sizeof(char)+1);
+      memcpy(tmp_func_item->param_types, '\0', 1);
       //tmp_func_item->param_types = NULL;
     }
   } else {
